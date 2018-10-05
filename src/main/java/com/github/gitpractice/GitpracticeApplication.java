96a2404b2d4dfc316c86e90f4a6888529726dbe6
Samuel Elias Garay Menjivar <samuelgaraymen@gmail.com>
2018-10-04 20:53:58 -0600

    Commit inicial, agregados archivos al proyecto

---
+++ src/main/java/com/github/gitpractice/GitpracticeApplication.java
@@ -0,0 +1,12 @@
+package com.github.gitpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpracticeApplication.class, args);
+	}
+}
